c16478e6eb45298915ea7d887e28e60b81c34072
Hevelon <halisson.hevelon@gmail.com>
2023-12-27 14:43:33 -0300

    modified:   application/views/vehicle_management.php

--- application/views/vehicle_management.php
+++ application/views/vehicle_management.php
@@ -2,7 +2,7 @@
    <div class="container-fluid">
       <div class="row mb-2">
          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Vehicle's Management
+            <h1 class="m-0 text-dark">Gestão de Veículos
             </h1>
          </div>
          <div class="col-sm-6">
